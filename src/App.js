3eb3d18c4e752f444b6a609274dffbd06e5c7b9a
MaliErdgn <105178494+MaliErdgn@users.noreply.github.com>
2023-03-17 04:25:50 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.css'
+import sections from "./sections.json";
+import Section from "./components/Section";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      <div className="container">
+        {sections.map((section) => (
+          <Section title={section.title} description={section.description} key={section.id} />
+        ))}
+      </div>
+    </>
   );
 }
 
